e5b2ddcde3b3baef77d078ceb73758cf00716a08
ObedRav <obedrayo2356@gmail.com>
2023-01-25 21:01:21 +0000

    Fixing errors

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+
 def uppercase(string):
     for i in range(len(string)):
         if ord(string[i]) >= 97 and ord(string[i]) <= 122:
             print("{}".format(chr(ord(string[i]) - 32)), end='')
         else:
             print("{}".format(string[i]), end='')
+    sys.stdout.write("\n")
