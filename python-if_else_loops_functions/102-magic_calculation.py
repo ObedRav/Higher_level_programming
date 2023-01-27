72ce70e8f49e4126dc340b925ca287989387b144
ObedRav <obedrayo2356@gmail.com>
2023-01-27 21:53:10 +0000

    Add magic calculation

--- python-if_else_loops_functions/102-magic_calculation.py
+++ python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
-    if a < b:
-        return c
-    elif b > c:
+    if b < c:
         return a + b
+    elif b > a:
+        return c
     else:
         return a * b - c
