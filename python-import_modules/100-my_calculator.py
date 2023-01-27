3424f53d314d72647a2b1e93abe8b03ddde6ecf8
ObedRav <obedrayo2356@gmail.com>
2023-01-27 21:22:26 +0000

    Add my_calculator

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 from calculator_1 import add, sub, mul, div
 
 __name__ = "__main__"
 
-argv = sys.argv
 allowed_operators = {"+": add, "-": sub, "*": mul, "/": div}
 
 
 if (len(argv) != 4):
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+    exit(1)
 elif (not argv[2] in allowed_operators):
     print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
+    exit(1)
 
 a = int(argv[1])
 b = int(argv[3])
