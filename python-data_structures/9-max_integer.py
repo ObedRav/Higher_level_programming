cf56bcf84b74d3d6dee95237f67e99f270ff65aa
ObedRav <obedrayo2356@gmail.com>
2023-01-30 16:33:16 +0000

    Add max integer

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    return_value = max(my_list) if my_list else None
+    my_list.sort()
+    return_value = my_list[-1] if my_list else None
     return return_value
