42cd64531a710770c097908dc7b25b6c0dca5144
ObedRav <obedrayo2356@gmail.com>
2023-01-30 23:52:21 +0000

    Add roman to int

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    try:
-        roman_numbers = \
-            {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        roman_int = 0
-        temp = roman_string[0]
-        for i in roman_string:
-            if (roman_numbers.get(temp) < roman_numbers.get(i)):
-                roman_int -= roman_numbers.get(temp) * 2
-            roman_int += roman_numbers.get(i)
-            temp = i
-        return roman_int
-    except TypeError:
+    if (not isinstance(roman_string, str) or roman_string is None):
         return 0
+    roman_numbers = \
+        {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_int = 0
+    temp = roman_string[0]
+    for i in roman_string:
+        if (roman_numbers.get(temp) < roman_numbers.get(i)):
+            roman_int -= roman_numbers.get(temp) * 2
+        roman_int += roman_numbers.get(i)
+        temp = i
+    return roman_int
