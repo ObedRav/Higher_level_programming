2f035925c229429a71d265deaf51446269f1940a
ObedRav <obedrayo2356@gmail.com>
2023-01-30 22:24:29 +0000

    roman to int

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_numbers = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    if (not roman_string):
+        return 0
+    roman_numbers = \
+        {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     roman_int = 0
     temp = ""
     for i in roman_string:
