f527c0f4addc908ece6544b98df9c346db170e0d
ObedRav <obedrayo2356@gmail.com>
2023-01-30 22:08:25 +0000

    Add best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,20 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if (not a_dictionary):
-        return None
-    temp = 0
-    best_score_student = ""
-    for key, value in a_dictionary.items():
-        if (temp < value):
-            best_score_student = key
-        temp = value
-    return best_score_student
-
-
-'''
-def best_score(a_dictionary):
     if not a_dictionary:
         return None
     best_key = max(a_dictionary, key=a_dictionary.get)
     return best_key
-'''
