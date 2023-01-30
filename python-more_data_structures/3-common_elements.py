eb0f878e2097b42c4f822ca4d4d865119ddd5aa2
ObedRav <obedrayo2356@gmail.com>
2023-01-30 21:03:21 +0000

    Add common elements

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    return set_1 & set_2
+
+
+'''def common_elements(set_1, set_2):
     common = set(filter(lambda x : x in set_1, set_2))
-    return common
+    return common'''
