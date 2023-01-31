17d8f77c90bf1817632508b1d5edc437f44017dc
ObedRav <obedrayo2356@gmail.com>
2023-01-31 23:02:42 +0000

    Add safe list

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -9,3 +9,6 @@ def safe_print_integer_err(value):
     except ValueError as err:
         print(f"Exception: {(err.args[0])}", file=sys.stderr)
         return False
+    except TypeError as err1:
+        print(f"Exception: {(err1.args[0])}", file=sys.stderr)
+        return False
