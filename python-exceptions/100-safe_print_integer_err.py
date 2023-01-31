bc1268d0c87a09d8333ad70a56163925e2ca2c90
ObedRav <obedrayo2356@gmail.com>
2023-01-31 22:51:51 +0000

    Add safe print

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import sys
+from sys import stderr as tderr
 
 
 def safe_print_integer_err(value):
+    string = "Exception: Unknown format code 'd'"
     try:
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError):
-        print("Exception: Unknown format code 'd'\
-             for object of type 'str'", file=sys.stderr)
+        tderr.write(f"{string} for object of type 'str'\n")
         return False
