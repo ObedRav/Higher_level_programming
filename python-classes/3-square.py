1ccf7158d74c3c7c3bae4d632412b627478ce2fd
ObedRav <obedrayo2356@gmail.com>
2023-02-08 21:02:04 +0000

    Add Square Class

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -6,10 +6,14 @@ class Square:
     """ This is a class that have size as a private property and
         run vallidations to the arguments"""
     def __init__(self, size=0):
-        """ Args: size (int): size of the square """
+        """ Attributes: size (int): size of the square """
         if not isinstance(size, int):  # If size is not an integer
             raise TypeError("size must be an integer")
         if size < 0:  # If size is negative
             raise ValueError("size must be >= 0")
 
         self.__size = size
+
+    def area(self) -> int:
+        """Function to calculate the area of the square"""
+        return self.__size * self.__size
