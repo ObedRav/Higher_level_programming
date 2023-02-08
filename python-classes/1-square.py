c9f7f9a9af4bb6ed5aaec726426f7eeae945c350
ObedRav <obedrayo2356@gmail.com>
2023-02-08 20:36:57 +0000

    Add square class with a private property

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" This module is about define a square """
+
+
+class Square:
+    """ This is a class that have size as a private property"""
+    def __init__(self, size):
+        self.__size = size
