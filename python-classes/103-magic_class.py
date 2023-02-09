6eb6b70ea181b57ba843fed30fba42ab920da1ed
ObedRav <obedrayo2356@gmail.com>
2023-02-09 01:49:39 +0000

    Add magic class

--- python-classes/103-magic_class.py
+++ python-classes/103-magic_class.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+""" This module is about a MagicClass to a Squeare """
 import math
 
 class MagicClass:
+    """ This class represents a magic class """
     def __init__(self, radius):
         self.__radius = 0
         if type(radius) not in (int, float):
