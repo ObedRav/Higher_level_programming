c21b2124e2f6c141f56f5a8381d3a5ba6daff88f
ObedRav <obedrayo2356@gmail.com>
2023-02-10 22:06:20 +0000

    Add text identation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""text indent"""
-
+"""This code test itself"""
 
 def text_indentation(text):
-    if not isinstance(text, str):
-        raise TypeError("text must be a string")
-
-    new_text = text.replace(".", ".\n\n")
-    new_text = new_text.replace("?", "?\n\n")
-    new_text = new_text.replace(":", ":\n\n")
-
-    new_text = text.replace(". ", ".\n\n")
-    new_text = new_text.replace("? ", "?\n\n")
-    new_text = new_text.replace(": ", ":\n\n")    
-
-
-    print(new_text.strip(), end="")
-
-
+    """This function print a new line if the text == .?:"""
+    i = 0
+    if type(text) != str:
+        raise TypeError('text must be a string')
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] == "\n" or text[i] in ".?:":
+            if text[i] in ".?:":
+                print("\n")
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+            continue
+        i += 1
