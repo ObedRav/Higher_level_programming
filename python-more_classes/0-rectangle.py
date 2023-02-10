fbf415124a3d13e6d33761584d86b2be8f63157e
ObedRav <obedrayo2356@gmail.com>
2023-02-10 19:05:51 +0000

    Add rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module Name: 0-rectangle
+
+Module Description:
+This module contains only one class
+
+Module Classes:
+- Rectangle
+
+Module Attributes:
+- None
+"""
+
+
+class Rectangle:
+    """Empty class to represent a rectangle"""
+    pass
