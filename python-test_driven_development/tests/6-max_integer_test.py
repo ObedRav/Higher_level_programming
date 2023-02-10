2f21cd2626c279626653e1448bde6d84ff11734b
ObedRav <obedrayo2356@gmail.com>
2023-02-10 22:29:10 +0000

    Add tests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -11,6 +11,20 @@ class TestMaxInteger(unittest.TestCase):
     def test_pass_0(self):
         self.assertEqual(max_integer([]), None)
 
-    def test_integers(self):
+    def test_integers_max_at_end(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_integers_max_at_mid(self):
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+
+    def test_integers_max_at_beg(self):
+        self.assertEqual(max_integer([9, 3, 4, 2]), 9)
+    
+    def test_integers_one_negative_number(self):
+        self.assertEqual(max_integer([9, 3, -4, 2]), 9)
+
+    def test_integers_only_negatives_numbers(self):
+        self.assertEqual(max_integer([-9, -3, -4, -2]), -2)
+
+    def test_integers_only_one_elemet(self):
+        self.assertEqual(max_integer([-2]), -2)
