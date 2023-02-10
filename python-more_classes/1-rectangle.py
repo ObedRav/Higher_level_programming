57ced8ef38516763103a170c969f5552e5fd9199
ObedRav <obedrayo2356@gmail.com>
2023-02-10 19:23:34 +0000

    Add rectagle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -22,9 +22,9 @@ class Rectangle:
         if width < 0:
             raise ValueError("width must be >= 0")
         if not isinstance(height, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if height < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
 
         self.__width = width
         self.__height = height
