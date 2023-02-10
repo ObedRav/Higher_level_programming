5729935ea1df4da3077a408cbd5d231924c8f3b4
ObedRav <obedrayo2356@gmail.com>
2023-02-10 15:52:06 +0000

    Add matrix divided

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,22 +7,20 @@ this function is add_integer
 
 
 def add_integer(a, b=98):
-    """ This function adds integer
-    and the second integer is formatted
-    to 98"""
+    """ This function adds two integers.
+    The second integer is set to 98 by default. 
+    """
 
-    if not isinstance(a, (int, float)) or a is None:
-        raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)) or b is None:
-        raise TypeError("b must be an integer")
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer or float")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer or float")
 
-    if a != a:
-        a = 89
-    if b != b:
-        b = 89
+    a = 89 if a != a else a
+    b = 89 if b != b else b
 
     result = a + b
-    if result == float('inf') or result == -float('inf'):
+    if result in [float('inf'), -float('inf')]:
         return 89
 
     return int(a) + int(b)
--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -4,8 +4,45 @@
 
 
 def matrix_divided(matrix, div):
-    new_matrix = []
-    for i in range(matrix):
-        for j in range(matrix[i]):
-            new_matrix.append(matrix[i][j])
+    """ This function divides all elements of a matrix by a number.
+
+    Args:
+    matrix (list of lists): A matrix of integers or floats
+    div (int or float): The number to divide the matrix by
+
+    Returns:
+    list of lists: The resulting matrix after division
+
+    Raises:
+    TypeError: If `div` is not a number, or if the elements of `matrix` are not integers or floats
+    ZeroDivisionError: If `div` is equal to 0
+    TypeError: If not all rows in `matrix` have the same length
+
+    Example:
+
+    >>> matrix_divided([[1, 2, 3], [4, 5, 6]], 2)
+    [[0.5, 1.0, 1.5], [2.0, 2.5, 3.0]]
+
+    >>> matrix_divided([[1, 2], [3, 4], [5, 6]], 2)
+    [[0.5, 1.0], [1.5, 2.0], [2.5, 3.0]]
+
+    >>> matrix_divided([[1, 2, 3], [4, 5, 6]], 0)
+    Traceback (most recent call last):
+        ...
+    ZeroDivisionError: division by zero
+    """
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    size = len(matrix[0])
+    for row in matrix:
+        if len(row) != size:
+            raise TypeError("Each row of the matrix must have the same size")
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers or floats")
+
+    new_matrix = [[round(element / div, 2) for element in row] for row in matrix]
     return new_matrix
