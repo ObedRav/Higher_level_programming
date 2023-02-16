3af85ad4c84659d65f5dee98bbd2fa025ccd013d
ObedRav <obedrayo2356@gmail.com>
2023-02-16 20:00:06 +0000

    Base geometry

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -85,7 +85,7 @@ class BaseGeometry:
         ...
         TypeError: size must be an integer
         """
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
