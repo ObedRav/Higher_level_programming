b4407c5482ce4de1055c307c68a43530a12ead33
ObedRav <obedrayo2356@gmail.com>
2023-02-20 23:57:10 +0000

    Add rectangle class

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -14,6 +14,29 @@ Module Attributes:
 from models.base import Base
 
 
+def check_integer(value, name):
+    """
+    Is a helper function to check if the passed parameter
+    is an integer and raise an appropriate error message if it is not.
+
+    Raises a TypeError if the value is not an integer.
+
+    Raises a ValueError if the value is less than or equal to 0 for width
+    and height properties, and less than 0 for x and y properties.
+    """
+    under_equals = ["width", "height"]
+    under = ["y", "x"]
+
+    if not isinstance(value, int):
+        raise TypeError(f"{name} must be an integer")
+    if name in under_equals:
+        if value <= 0:
+            raise ValueError(f"{name} must be > 0")
+    if name in under:
+        if value < 0:
+            raise ValueError(f"{name} must be >= 0")
+
+
 class Rectangle(Base):
     """
     Represents a rectangle with dimensions and coordinates,
@@ -40,6 +63,10 @@ class Rectangle(Base):
         id: An integer representing the unique identifier of the rectangle.
             Default is None.
         """
+        check_integer(width, "width")
+        check_integer(height, "height")
+        check_integer(x, "x")
+        check_integer(y, "y")
         super().__init__(id)
         self.__width = width
         self.__height = height
@@ -48,40 +75,110 @@ class Rectangle(Base):
 
     @property
     def width(self) -> int:
-            """Returns the value of the __width attribute"""
-            return self.__width
+        """Returns the value of the __width attribute"""
+        return self.__width
 
     @width.setter
     def width(self, value: int):
-            """Sets the value of the __width attribute"""
-            self.__width = value
+        """Sets the value of the __width attribute"""
+        check_integer(value, "width")
+        self.__width = value
 
     @property
     def height(self) -> int:
-            """Returns the value of the __height attribute"""
-            return self.__height
+        """Returns the value of the __height attribute"""
+        return self.__height
 
     @height.setter
     def height(self, value: int):
-            """Sets the value of the __height attribute"""
-            self.__height = value
+        """Sets the value of the __height attribute"""
+        check_integer(value, "height")
+        self.__height = value
 
     @property
     def x(self) -> int:
-            """Returns the value of the __x attribute"""
-            return self.__x
+        """Returns the value of the __x attribute"""
+        return self.__x
 
     @x.setter
     def x(self, value: int):
-            """Sets the value of the __x attribute"""
-            self.__x = value
+        """Sets the value of the __x attribute"""
+        check_integer(value, "x")
+        self.__x = value
 
     @property
     def y(self) -> int:
-            """Returns the value of the __y attribute"""
-            return self.__y
+        """Returns the value of the __y attribute"""
+        return self.__y
 
     @y.setter
     def y(self, value: int):
-            """Sets the value of the __y attribute"""
-            self.__y = value
+        """Sets the value of the __y attribute"""
+        check_integer(value, "y")
+        self.__y = value
+
+    def area(self) -> float:
+        """
+        Method that calculates the area of the rectangle and returns it as a float.
+        """
+        return self.__width * self.__height
+
+    def display(self) -> None:
+        """
+        Method that prints out the rectangle using the print function.
+        It first prints out the correct number of newlines based on the
+        value of __y. Then, for each row of the rectangle, it prints out
+        the correct number of spaces based on the value of __x, followed
+        by the correct number of # characters based on the value of __width.
+        """
+        for k in range(self.__y):
+            print()
+        for i in range(self.__height):
+            for l in range(self.__x):
+                    print(" ", end="")
+            for j in range(self.__width):
+                print("#", end="")
+            print()
+
+    def __str__(self) -> str:
+        """
+        Method that returns a string representation of the rectangle.
+        The string is in the format [Rectangle] (<id>) <x>/<y> - <width>/<height>,
+        where <id>, <x>, <y>, <width>, and <height> are replaced with the actual
+        values of the corresponding instance variables.
+        """
+        return_value = f"[Rectangle] ({self.id}) {self.__x}/{self.__y}"
+        return_value = f"{return_value} - {self.__width}/{self.__height}"
+        return return_value
+
+    def update(self, *args, **kwargs) -> None:
+        """
+        Update the rectangle attributes.
+
+        Args:
+        *args: Variable length argument list representing the attributes in the following order:
+            - id (optional): int
+            - width (optional): int
+            - height (optional): int
+            - x (optional): int
+            - y (optional): int
+            Only the attributes included in the argument list will be updated.
+        **kwargs: Arbitrary keyword arguments representing the attributes and their values.
+            Only the attributes included in the keyword argument list will be updated.
+
+        Raises:
+            TypeError: If any of the arguments have an incorrect type.
+            ValueError: If any of the arguments have an incorrect value.
+
+        Returns:
+            None.
+        """
+        attrs = ['id', 'width', 'height', 'x', 'y']
+        if args:
+            for attr, value in zip(attrs, args):
+                setattr(self, attr, value)
+        else:
+            
+            for key, value in kwargs.items():
+                if key in attrs:
+                    self.__setattr__(key, value)
