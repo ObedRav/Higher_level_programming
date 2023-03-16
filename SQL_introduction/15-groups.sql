96fffa9b2d3fcc6cfa483adf7c001362e745bedf
ObedRav <obedrayo2356@gmail.com>
2023-03-16 19:01:21 +0000

    Fixing a bug

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- listing the number of records with the same score in the table second_table
-SELECT score, COUNT(score) as number FROM second_table GROUP BY score DESC;
+SELECT score, COUNT(score) as number FROM second_table GROUP BY score ORDER BY score DESC;
