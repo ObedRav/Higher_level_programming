0e6e2f4aa4d97503e888e41aa89efceb4844e821
ObedRav <obedrayo2356@gmail.com>
2023-03-16 18:34:23 +0000

    Adding comments

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,9 +1,11 @@
+-- creating the table second_table in the database hbtn_0c_0
 CREATE TABLE IF NOT EXISTS second_table(
     id INT,
     name VARCHAR(256),
     score INT
 );
 
+-- creating these records
 INSERT INTO second_table (id, name, score)
 VALUES
     (1, "John", 10),
