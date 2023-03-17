76eab0870c69ff48c71776505fc8ce885afbb0c1
ObedRav <obedrayo2356@gmail.com>
2023-03-17 19:32:55 +0000

    Adding safe filter

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Module Name: 0-select_states
+
+Module Description:
+This module contains only one function
+
+Module Functions:
+- sol() -> None
+
+Module Attributes:
+- None
+"""
+import MySQLdb
+from sys import argv
+
+
+def solution():
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+    state_name = argv[4].split(';')[0].strip("'")
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database_name,
+        charset="utf8")
+
+    cur = conn.cursor()
+    cur.execute("""
+    SELECT id, name
+    FROM states
+    WHERE name = '{}' AND BINARY name = BINARY '{}'
+    ORDER BY id;
+    """.format(state_name, state_name))
+
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    solution()
