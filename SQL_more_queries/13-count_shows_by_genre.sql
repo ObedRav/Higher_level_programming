74588d0279d0f78fcdcf34d3245d23c05630342d
ObedRav <obedrayo2356@gmail.com>
2023-03-17 16:32:01 +0000

    Adding count shows by genre

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- Listing all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres
+LEFT JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_show_genres.genre_id IS NOT NULL
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
