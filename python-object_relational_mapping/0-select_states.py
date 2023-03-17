60ee90c01ec961b1c43dff00be1b3b8f2646c4e5
ObedRav <obedrayo2356@gmail.com>
2023-03-17 18:57:43 +0000

    Pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -14,19 +14,30 @@ Module Attributes:
 import MySQLdb
 from sys import argv
 
+
 def sol():
     username = argv[1]
     password = argv[2]
     database_name = argv[3]
 
-    conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name, charset="utf8")
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database_name,
+        charset="utf8")
+
     cur = conn.cursor()
     cur.execute("SELECT id, name FROM states ORDER BY id")
+
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
+
     cur.close()
     conn.close()
+
 
 if __name__ == '__main__':
     sol()
