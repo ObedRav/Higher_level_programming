53e0a9fb1a1487a4e2a0dba43fb0992e0f7384ea
ObedRav <obedrayo2356@gmail.com>
2023-03-17 19:17:04 +0000

    Pycodestyle

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -29,7 +29,12 @@ def solution():
         charset="utf8")
 
     cur = conn.cursor()
-    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' AND BINARY LEFT(name, 1) = BINARY 'N' ORDER BY id")
+    cur.execute("""
+    SELECT id, name
+    FROM states
+    WHERE name LIKE 'N%' AND BINARY LEFT(name, 1) = BINARY 'N'
+    ORDER BY id
+    """)
 
     query_rows = cur.fetchall()
     for row in query_rows:
