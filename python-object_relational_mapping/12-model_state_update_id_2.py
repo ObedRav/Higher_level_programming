66b53d85a33a1ab202f4843a50ec3cc5c507de81
ObedRav <obedrayo2356@gmail.com>
2023-03-17 22:02:24 +0000

    Adding update id 2

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Module Name: 7-model_state_fetch_all
+
+Module Description:
+This module contains only one function
+
+Module Functions:
+- Solution() -> None
+
+Module Attributes:
+- None
+"""
+import sys
+
+from model_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def Solution():
+    """Prints all State objects that contain the letter a from the database."""
+    # Desestructuring the args
+    username, password, database_name = sys.argv[1:4]
+
+    # Creating database connection
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        username,
+        password,
+        database_name),
+        pool_pre_ping=True)
+
+    # Create a session factory
+    Session = sessionmaker(bind=engine)
+
+    # Use the with to handle the close session
+    with Session() as session:
+        # Creating a new instance
+        session.query(State).filter(State.id == 2).update({State.name: "New Mexico"})
+
+        # commit the transaction
+        session.commit()
+
+
+if __name__ == "__main__":
+    Solution()
