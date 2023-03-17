633f0e52d121ec5c3c09922a4888287aa2e380bc
ObedRav <obedrayo2356@gmail.com>
2023-03-17 22:52:57 +0000

    Pycodestyle

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -23,23 +23,31 @@ from sqlalchemy.orm import sessionmaker
 
 
 def Solution():
-    """Prints all City objects related to a specific State object from the database."""
+    """
+    Prints all City objects related to a
+    specific State object from the database
+    """
     try:
         # Destructure the args
         username, password, database_name = sys.argv[1:4]
 
         # Create a database connection
-        engine = create_engine(f"mysql+mysqldb://{username}:{password}@localhost/{database_name}", pool_pre_ping=True)
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username,
+            password,
+            database_name),
+            pool_pre_ping=True)
 
         # Create a session factory
         Session = sessionmaker(bind=engine)
 
-        # Use the with statement to handle the session and connection automatically
+        # Use the with statement to handle the
+        # session and connection automatically
         with Session() as session:
             for city, state in session.query(City, State) \
                               .filter(City.state_id == State.id) \
                               .order_by(City.id):
-             print("{}: ({}) {}".format(state.name, city.id, city.name))
+                print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     except Exception as e:
         print(f"An error occurred: {e}")
