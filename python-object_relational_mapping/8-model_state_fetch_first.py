2e5b7fa5acf18bf3193e256a8f12b0922226c52c
ObedRav <obedrayo2356@gmail.com>
2023-03-17 21:19:47 +0000

    Adding fetch first

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Module Name: 7-model_state_fetch_all
+
+Module Description:
+This module contains only one function
+
+Module Functions:
+- Solution() -> None
+
+Module Attributes:
+- None
+"""
+import sys
+
+from model_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def Solution():
+    """Prints all the states from the database."""
+    # Creating database connection
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]),
+        pool_pre_ping=True)
+
+    # Create a session factory
+    Session = sessionmaker(bind=engine)
+
+    # Use the with to handle the close session
+    with Session() as session:
+        # Query the database for all states
+        state = session.query(State).first()
+
+        # Print the results
+        print(f"{state.id}: {state.name}")
+
+
+if __name__ == "__main__":
+    Solution()
