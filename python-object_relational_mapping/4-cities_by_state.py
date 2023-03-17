bafad8eba0a790e17c34b10d637e5be2db10dc6a
ObedRav <obedrayo2356@gmail.com>
2023-03-17 19:42:15 +0000

    Adding cities by state

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Module Name: 0-select_states
+
+Module Description:
+This module contains only one function
+
+Module Functions:
+- sol() -> None
+
+Module Attributes:
+- None
+"""
+import MySQLdb
+from sys import argv
+
+
+def solution():
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database_name,
+        charset="utf8")
+
+    cur = conn.cursor()
+    cur.execute("""
+    SELECT cities.id, cities.name, states.name
+    FROM cities
+    INNER JOIN states
+    ON cities.state_id = states.id
+    ORDER BY cities.id
+    """)
+
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    solution()
