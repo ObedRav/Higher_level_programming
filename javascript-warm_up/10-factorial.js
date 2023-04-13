11e6a7d306341300be22e49652e2100889ce2b9d
ObedRav <obedrayo2356@gmail.com>
2023-04-13 16:17:39 +0000

    Adding factorial

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-function recursiveFactorial(num) {
+function recursiveFactorial (num) {
   if (num === 1) {
     return 1;
   } else {
     return num * recursiveFactorial(num - 1);
   }
-};
+}
 
 const number = parseInt(process.argv[2]) || 1;
 
