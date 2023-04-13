0d347da1391ea76d2f395088c06287965398f15d
ObedRav <obedrayo2356@gmail.com>
2023-04-13 15:34:29 +0000

    Adding to_integer

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (parseInt(process.argv.slice(2)[0])) {
-  console.log(parseInt(process.argv.slice(2)[0]));
+  console.log(`My number: ${parseInt(process.argv.slice(2)[0])}`);
 } else {
   console.log('Not a number');
 }
