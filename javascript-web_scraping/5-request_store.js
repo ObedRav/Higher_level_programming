b21abe478572f1f6db680d72de6523f41af5fd03
ObedRav <obedrayo2356@gmail.com>
2023-04-18 18:48:41 -0500

    Adding request store

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+request.get(process.argv[2], function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFileSync(process.argv[3], body);
+  }
+});
