ee22e9d4c3a5a427bfad75f3cc2141ece7b9707c
ObedRav <obedrayo2356@gmail.com>
2023-04-18 12:25:59 -0500

    Fixing some bugs

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -5,6 +5,6 @@ const fs = require('fs');
 const firstFile = fs.readFileSync(process.argv[2], 'utf8');
 const secondFile = fs.readFileSync(process.argv[3], 'utf8');
 
-const data = `${firstFile.replace('\n', '')}\n${secondFile}`;
+const data = `${firstFile}${secondFile}`;
 
 fs.writeFileSync(process.argv[4], data);
